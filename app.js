e063c8e28717fbe323532c3023e4703dfd5e068d
Matheus <MPestudante12@gmail.com>
2024-12-04 11:16:50 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict'
+const switcher = document.querySelector('.btn');
+switcher.addEventListener('click', function () {
+    if (document.body.classList.contains('light-theme')) {
+        document.body.classList.remove('light-theme');
+        document.body.classList.add('dark-theme');
+        this.textContent = "Light";
+    } else {
+        document.body.classList.remove('dark-theme');
+        document.body.classList.add('light-theme');
+        this.textContent = "Dark";
+    }
+    console.log('Current class name: ' + document.body.className);
+});
